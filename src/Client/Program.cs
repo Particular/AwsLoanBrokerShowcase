a9693dfafc9cdc450fdd2dfd731ac30afd1dca51
Sara Pellegrini <sara.pellegrini@gmail.com>
2024-04-23 11:50:03 +0200

    Creation of the client project for sending initial commands.

---
+++ src/Client/Program.cs
@@ -0,0 +1,73 @@
+using Amazon.Runtime;
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
+using Messages;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+// To create a docker container, use the following command: dotnet publish /t:PublishContainer
+// See https://learn.microsoft.com/en-us/dotnet/core/docker/publish-as-container?pivots=dotnet-8-0#publish-net-app for details
+var builder = Host.CreateApplicationBuilder(args);
+
+// TODO: consider moving common endpoint configuration into a shared project
+// for use by all endpoints in the system
+
+var endpointConfiguration = new EndpointConfiguration("Client");
+
+var localStackEdgeUrl = "http://localhost:4566";
+var emptyLocalStackCredentials = new BasicAWSCredentials("xxx", "xxx");
+var sqsConfig = new AmazonSQSConfig() { ServiceURL = localStackEdgeUrl };
+var snsConfig = new AmazonSimpleNotificationServiceConfig() { ServiceURL = localStackEdgeUrl };
+
+var transport = new SqsTransport(
+    new AmazonSQSClient(emptyLocalStackCredentials, sqsConfig),
+    new AmazonSimpleNotificationServiceClient(emptyLocalStackCredentials, snsConfig));
+var routingSettings = endpointConfiguration.UseTransport(transport);
+routingSettings.RouteToEndpoint(typeof(FindBestLoan), "LoanBroker");
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.DefineCriticalErrorAction(OnCriticalError);
+endpointConfiguration.EnableInstallers();
+
+
+builder.UseNServiceBus(endpointConfiguration);
+
+var app = builder.Build();
+
+await app.StartAsync();
+var messageSession = app.Services.GetRequiredService<IMessageSession>();
+var requestId = Guid.NewGuid().ToString().Substring(0, 8);
+var prospect = new Prospect("Scrooge", "McDuck");
+await messageSession.Send(new FindBestLoan(requestId, prospect, 10, 1000));
+await app.StopAsync();
+
+
+
+static async Task OnCriticalError(ICriticalErrorContext context, CancellationToken cancellationToken)
+{
+    // TODO: decide if stopping the endpoint and exiting the process is the best response to a critical error
+    // https://docs.particular.net/nservicebus/hosting/critical-errors
+    // and consider setting up service recovery
+    // https://docs.particular.net/nservicebus/hosting/windows-service#installation-restart-recovery
+    try
+    {
+        await context.Stop(cancellationToken);
+    }
+    finally
+    {
+        FailFast($"Critical error, shutting down: {context.Error}", context.Exception);
+    }
+}
+
+static void FailFast(string message, Exception exception)
+{
+    try
+    {
+        // TODO: decide what kind of last resort logging is necessary
+        // TODO: when using an external logging framework it is important to flush any pending entries prior to calling FailFast
+        // https://docs.particular.net/nservicebus/hosting/critical-errors#when-to-override-the-default-critical-error-action
+    }
+    finally
+    {
+        Environment.FailFast(message, exception);
+    }
+}
