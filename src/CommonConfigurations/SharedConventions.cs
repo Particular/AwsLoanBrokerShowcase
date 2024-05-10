a381d76473b2d1fba5ab25cf755fada7dbe2ba91
Sara Pellegrini <sara.pellegrini@gmail.com>
2024-05-10 14:36:45 +0200

    Basic implementation of Tracing and Metrics with OpenTelemetry

--- src/CommonConfigurations/SharedConventions.cs
+++ src/CommonConfigurations/SharedConventions.cs
@@ -1,12 +1,18 @@
 ﻿using Amazon.Runtime;
 using Amazon.SimpleNotificationService;
 using Amazon.SQS;
+using NServiceBus.Configuration.AdvancedExtensibility;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace CommonConfigurations;
 
 public static class SharedConventions
 {
-    public const string LocalStackEdgeUrl = "http://localhost:4566";
+    public const string LocalStackEdgeUrl = "http://localstack:4566";
     public static readonly AWSCredentials EmptyLocalStackCredentials = new BasicAWSCredentials("xxx", "xxx");
 
     public static RoutingSettings UseCommonTransport(this EndpointConfiguration endpointConfiguration)
@@ -27,5 +33,54 @@ public static class SharedConventions
         endpointConfiguration.CustomDiagnosticsWriter((_, _) => Task.CompletedTask);
         endpointConfiguration.UseSerialization<SystemJsonSerializer>();
         endpointConfiguration.EnableInstallers();
+        EnableMetrics(endpointConfiguration);
+        EnableTracing(endpointConfiguration);
+    }
+
+
+    static void EnableMetrics(EndpointConfiguration endpointConfiguration)
+    {
+        var endpointName = endpointConfiguration.GetSettings().EndpointName();
+        var attributes = new Dictionary<string, object>
+        {
+            ["service.name"] = endpointName,
+            ["service.instance.id"] = Guid.NewGuid().ToString(),
+        };
+
+        var resourceBuilder = ResourceBuilder.CreateDefault().AddAttributes(attributes);
+
+        Sdk.CreateMeterProviderBuilder()
+            .SetResourceBuilder(resourceBuilder)
+            .AddMeter("NServiceBus.Core")
+            .AddPrometheusHttpListener(options => options.UriPrefixes = new[] { "http://*:9464/" })
+            .Build();
+
+        endpointConfiguration.EnableOpenTelemetry();
+    }
+
+    static void EnableTracing(EndpointConfiguration endpointConfiguration)
+    {
+        var endpointName = endpointConfiguration.GetSettings().EndpointName();
+
+        var attributes = new Dictionary<string, object>
+        {
+            ["service.name"] = endpointName,
+            ["service.instance.id"] = Guid.NewGuid().ToString(),
+        };
+
+        var resourceBuilder = ResourceBuilder.CreateDefault().AddAttributes(attributes);
+
+        Sdk.CreateTracerProviderBuilder()
+            .SetResourceBuilder(resourceBuilder)
+            .AddSource("NServiceBus.Core")
+            .AddOtlpExporter(cfg =>
+            {
+                cfg.Endpoint = new Uri("http://jeager:4318/v1/traces");
+                cfg.Protocol = OtlpExportProtocol.HttpProtobuf;
+            })
+            .AddConsoleExporter()
+            .Build();
+
+        endpointConfiguration.EnableOpenTelemetry();
     }
 }
