f13061bf576f6c0aa230fe913f275a7b9b9d2cf2
sara pellegrini <sara.pellegrini@gmail.com>
2024-08-20 14:51:50 +0200

    Score lambda implementation.

--- src/lambdas/creditbureau.js
+++ src/lambdas/creditbureau.js
@@ -3,29 +3,30 @@
 }
 
 exports.score = async (event) => {
-    return "hello";
-    // const min_score = 300;
-    // const max_score = 900;
-    //
-    //
-    // var ssn_regex = new RegExp("^\\d{3}-\\d{2}-\\d{4}$");
-    // if (ssn_regex.test(event.SSN)) {
-    //     return {
-    //         statusCode: 200,
-    //         request_id: event.RequestId,
-    //         body: {
-    //             SSN: event.SSN,
-    //             score: getRandomInt(min_score, max_score),
-    //             history: getRandomInt(1,30)
-    //         }
-    //     };
-    // } else {
-    //     return {
-    //         statusCode: 400,
-    //         request_id: event.RequestId,
-    //         body: {
-    //             SSN: event.SSN
-    //         }
-    //     };
-    // }
+    const min_score = 300;
+    const max_score = 900;
+
+    var ssn_regex = new RegExp("^\\d{3}-\\d{2}-\\d{4}$");
+    let param = event.queryStringParameters;
+
+    if (ssn_regex.test(param.SSN)) {
+        return {
+            statusCode: 200,
+            request_id: param.RequestId,
+            body: {
+                SSN: param.SSN,
+                score: getRandomInt(min_score, max_score),
+                history: getRandomInt(1, 30)
+            }
+        };
+    } else {
+        return {
+            statusCode: 400,
+            request_id: param.RequestId,
+            body: {
+                SSN: param.SSN
+            }
+        };
+    }
 };
+
