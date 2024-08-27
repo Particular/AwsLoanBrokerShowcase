3a77263740a83056cf475e1bffde178b79010fd2
sara pellegrini <sara.pellegrini@gmail.com>
2024-08-27 12:28:38 +0200

    Add first template for terraform configuration.

---
+++ src/terraform/main.tf
@@ -0,0 +1,32 @@
+﻿terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_sqs_queue" "terraform_queue" {
+  name                      = "terraform-example-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+# Create docker containers for applications
+# Create labmda
+# invoke transport cli to configure the transport
+# create an empty dynamoDB
