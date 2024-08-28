ed0da926c07474fbcf1c03974c11a340334b71e9
sara pellegrini <sara.pellegrini@gmail.com>
2024-08-28 09:27:24 +0200

    Remove queue test. Add lambda and relative role. Add ECR repository.

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -13,18 +13,40 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_sqs_queue" "terraform_queue" {
-  name                      = "terraform-example-queue"
-  delay_seconds             = 90
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-
-
-  tags = {
-    Environment = "production"
-  }
+resource "aws_ecr_repository" "loan_broker_ecr_repo" {
+  name = "loan_broker-repo"
 }
+
+resource "aws_iam_role" "iam_for_loan_broker_lambda" {
+  name = "iam_for_loan_broker_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "loan_broker_credit_score_lambda" {
+  filename      = "../lambdas/function.zip"
+  function_name = "score"
+  role          = aws_iam_role.iam_for_loan_broker_lambda.arn
+  handler       = "creditbureau.score"
+  runtime       = "nodejs18.x"
+  memory_size   = 1024
+  timeout       = 300
+}
+
 
 # Create docker containers for applications
 # Create labmda
