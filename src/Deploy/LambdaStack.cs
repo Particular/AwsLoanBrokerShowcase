5e119c859e6b2c5232d5305b2f04927e00d8bd55
Mauro Servienti <mauro.servienti@gmail.com>
2024-08-29 12:18:36 +0200

    Add an example of Role, Policy, and Permission

--- src/Deploy/LambdaStack.cs
+++ src/Deploy/LambdaStack.cs
@@ -1,4 +1,5 @@
 ﻿using Amazon.CDK;
+using Amazon.CDK.AWS.IAM;
 using Amazon.CDK.AWS.Lambda;
 
 namespace Deploy;
@@ -8,20 +9,47 @@ class LambdaStack : Stack
     public LambdaStack(Construct scope, string id, IStackProps? props = null)
         : base(scope, id, props)
     {
-        var lambdaPath = Path.GetFullPath(Path.Combine(System.Environment.CurrentDirectory, "..", "..", "..", "..", "lambdas"));
+        var role = new Role(this, "CreditCheckRole", new RoleProps
+        {
+            InlinePolicies =
+            {
+                { "iam_for_loan_broker_lambda", new PolicyDocument(new PolicyDocumentProps()
+                {
+                    Statements =
+                    [
+                        new PolicyStatement(new PolicyStatementProps()
+                        {
+                            Actions = ["sts:AssumeRole"],
+                            Principals = [new ServicePrincipal("lambda.amazonaws.com")],
+                            Effect = Effect.ALLOW
+                        })
+                    ]
+                })}
+            }
+        });
 
+        var lambdaPath = Path.GetFullPath(Path.Combine(System.Environment.CurrentDirectory, "..", "..", "..", "..", "lambdas"));
         var lambdaFn = new Function(this, "CreditCheck", new FunctionProps
         {
             Runtime = new Runtime("NODEJS_20_X"),
             Code = Code.FromAsset(lambdaPath),
             Handler = "creditbureau.score",
-            Timeout = Duration.Seconds(30)
+            Timeout = Duration.Seconds(30),
+            Role = role
         });
 
-        var urlConfig = new FunctionUrl(this, "CreditCheckUrl", new FunctionUrlProps
+        _ = new FunctionUrl(this, "CreditCheckUrl", new FunctionUrlProps
         {
             AuthType = FunctionUrlAuthType.NONE,
             Function = lambdaFn
         });
+
+        _ = new Permission()
+        {
+            FunctionUrlAuthType = FunctionUrlAuthType.NONE,
+            Principal = new AccountPrincipal("*"),
+            Action = "lambda:InvokeFunctionUrl",
+            Scope = lambdaFn
+        };
     }
 }
