e948efbd17828bf343299c526f0c7e240551feda
Mauro Servienti <mauro.servienti@gmail.com>
2024-09-09 15:17:02 +0200

    Move LoanBrokerStack to its file

---
+++ src/Deploy/LoanBrokerStack.cs
@@ -0,0 +1,27 @@
+using Amazon.CDK;
+using Amazon.CDK.AWS.SQS;
+using BankMessages;
+using Deploy;
+
+class LoanBrokerStack : Stack
+{
+
+    public LoanBrokerStack(Construct scope, string id, IStackProps? props = null)
+        : base(scope, id, props)
+    {
+        _ = new NServiceBusEndpointResource(new EndpointDetails("LoanBroker"), this, "LoanBroker.LoanBroker");
+        _ = new NServiceBusEndpointResource(new EndpointDetails("Client"), this, "LoanBroker.Client");
+        var endpointDetails = new EndpointDetails("Bank1Adapter");
+        endpointDetails.EventsToSubscribe = [typeof(QuoteRequested)];
+        _ = new NServiceBusEndpointResource(endpointDetails, this, "LoanBroker.Bank1Adapter");
+        _ = new NServiceBusEndpointResource(new EndpointDetails("Bank2Adapter"), this, "LoanBroker.Bank2Adapter");
+        _ = new NServiceBusEndpointResource(new EndpointDetails("Bank3Adapter"), this, "LoanBroker.Bank3Adapter");
+
+        _ = new Queue(this, "error", new QueueProps
+        {
+            QueueName =  "error",
+            RetentionPeriod = Duration.Seconds(900)
+        });
+    }
+
+}
--- src/Deploy/Program.cs
+++ src/Deploy/Program.cs
@@ -1,7 +1,4 @@
 ﻿using Amazon.CDK;
-using Amazon.CDK.AWS.SQS;
-using BankMessages;
-using Deploy;
 
 var app = new App();
 
@@ -9,27 +6,3 @@ new LoanBrokerStack(app, "LoanBroker");
 //new LambdaStack(app, "CreditBureau", new StackProps());
 
 var cloudAssembly = app.Synth();
-
-
-class LoanBrokerStack : Stack
-{
-
-    public LoanBrokerStack(Construct scope, string id, IStackProps? props = null)
-        : base(scope, id, props)
-    {
-        new NServiceBusEndpointResource(new EndpointDetails("LoanBroker"), this, "LoanBroker.LoanBroker");
-        new NServiceBusEndpointResource(new EndpointDetails("Client"), this, "LoanBroker.Client");
-        var endpointDetails = new EndpointDetails("Bank1Adapter");
-        endpointDetails.EventsToSubscribe = [typeof(QuoteRequested)];
-        new NServiceBusEndpointResource(endpointDetails, this, "LoanBroker.Bank1Adapter");
-        new NServiceBusEndpointResource(new EndpointDetails("Bank2Adapter"), this, "LoanBroker.Bank2Adapter");
-        new NServiceBusEndpointResource(new EndpointDetails("Bank3Adapter"), this, "LoanBroker.Bank3Adapter");
-
-        var error = new Queue(this, "error", new QueueProps
-        {
-            QueueName =  "error",
-            RetentionPeriod = Duration.Seconds(900)
-        });
-    }
-
-}
